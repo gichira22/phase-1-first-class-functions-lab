7a3c1f50ce73d84878d9949b87d5e17664ceae7d
Virginia Gichira <gichiravirginia@gmail.com>
2024-03-30 16:23:19 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const fareQuintupler = createFareMultiplier(5);
+
+function selectDifferentDrivers(drivers, fn) {
+  return fn(drivers);
+}
